dfb0cb3966789455ccb0dc42ea1461a1d8897458
Владимир <141308863+ragingroland@users.noreply.github.com>
2024-05-26 03:11:31 +0600

    some updates

--- dags/university.py
+++ dags/university.py
@@ -12,9 +12,9 @@ def task_university_e(ti): # Забираем список словарей
     pd_data = pd.DataFrame(response)
 
     # Удаляем ненужные столбцы
-    pd_data = pd_data.drop(columns=['domains', 'web_pages'])
+    pd_data = pd_data.drop(columns = ['domains', 'web_pages'])
 
-    # Добавляем столбец uni_type
+    # Добавляем и заполняем столбец uni_type
     pd_data['uni_type'] = pd_data['name']
     for index, row in pd_data.iterrows():
         if 'College' in row['name']:
@@ -27,18 +27,18 @@ def task_university_e(ti): # Забираем список словарей
             pd_data.at[index, 'uni_type'] = ''
     
     # Переименовываем колонку
-    pd_data = pd_data.rename(columns={'state-province': 'state_province'})
+    pd_data = pd_data.rename(columns = {'state-province': 'state_province'})
 
     # Передаем данные в следующий таск уже как словарь в виде столбец-значение
     # иначе возникают ошибки индексов или сериализации
-    ti.xcom_push(key='university_data', value=pd_data.to_dict(orient='records'))
+    ti.xcom_push(key = 'university_data', value = pd_data.to_dict(orient = 'records'))
 
 def insert_data_into_db(**kwargs): # Создаем подключение к локальной БД PgSQL
-    pg_hook = PostgresHook(postgres_conn_id='postgres')
+    pg_hook = PostgresHook(postgres_conn_id = 'postgres')
     connection = pg_hook.get_conn()
     cursor = connection.cursor()
     # Забираем из XCom pd_data
-    data = kwargs['ti'].xcom_pull(key='university_data', task_ids='uni_data_transform')
+    data = kwargs['ti'].xcom_pull(key = 'university_data', task_ids = 'uni_data_transform')
     
     # Указываем в какие столбцы нужно вставить данные, значения указаны в виде заглушек
     # которые примут значения из переменной row[]
@@ -54,17 +54,17 @@ def insert_data_into_db(**kwargs): # Создаем подключение к л
     cursor.close()
     connection.close()
 
-# Инициализация ДАГа
+# Инициализация
 with DAG(
-    dag_id='university_test_task',
-    description='Universities domain ETL',
-    schedule_interval= '0 0 * * *',
-    start_date=datetime.combine(datetime.today(), datetime.min.time()),
-    catchup=False,
+    dag_id = 'university_test_task',
+    description = 'Universities domain ETL',
+    schedule_interval = '0 0 * * *',
+    start_date = datetime(2024, 5, 27),
+    catchup = False,
 ) as dag: # Создается таблица в PostgreSQL
     task_create_pgsql_table = PostgresOperator(
-        task_id='create_pgsql_table',
-        sql="""
+        task_id = 'create_pgsql_table',
+        sql = """
             CREATE TABLE IF NOT EXISTS university_domain (
             uni_id SERIAL PRIMARY KEY,
             alpha_two_code VARCHAR(2) NULL,
@@ -74,19 +74,19 @@ with DAG(
             uni_type VARCHAR(50) NULL,
             UNIQUE (name, country));
         """,
-        postgres_conn_id='postgres'
+        postgres_conn_id = 'postgres'
     )
-    # Таска по преобразованию данных
+    # Задача по преобразованию данных
     transforming_uni_data = PythonOperator(
-        task_id='uni_data_transform',
-        python_callable=task_university_e,
-        provide_context=True
+        task_id = 'uni_data_transform',
+        python_callable = task_university_e,
+        provide_context = True
     )
-    # Таска по загрузке данных в базу
+    # Задача по загрузке данных в базу
     loading_uni_data = PythonOperator(
-        task_id='load_uni_data_to_db',
-        python_callable=insert_data_into_db,
-        provide_context=True
+        task_id = 'load_uni_data_to_db',
+        python_callable = insert_data_into_db,
+        provide_context = True
     )
 
     task_create_pgsql_table >> transforming_uni_data >> loading_uni_data
